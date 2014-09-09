51c9e84d8bdffafaab1980e962e5ce8bec66b0a2
soda <soda@mk.ces.kyutech.ac.jp>
2014-09-09 16:50:52 +0900

    add test Mikity3dAnimation

---
+++ mikity3D-swt/src/test/java/org/mklab/mikity/Mikity3dAnimationTest.java
@@ -0,0 +1,28 @@
+/*
+ * Created on 2014/09/04
+ * Copyright (C) 2014 Koga Laboratory. All rights reserved.
+ *
+ */
+package org.mklab.mikity;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.mklab.mikity.view.gui.AnimationWindow;
+
+
+/**
+ * @author soda
+ * @version $Revision$, 2014/09/04
+ * Mikity3dAnimationのためのテストクラスです。
+ */
+public class Mikity3dAnimationTest {
+
+  @Test
+  public void testModelFileOpen() {
+    AnimationWindow aw = new AnimationWindow(null);
+   // assertThat(, is(".mat"));
+    
+  }
+
+}
