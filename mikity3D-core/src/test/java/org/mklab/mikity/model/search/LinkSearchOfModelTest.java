d834a62214301bc16e7bafdd24ef93fd55c7a1d6
soda <soda@mk.ces.kyutech.ac.jp>
2015-01-28 15:07:00 +0900

    Added LinkSearchOfModel class and test code.

---
+++ mikity3D-core/src/test/java/org/mklab/mikity/model/search/LinkSearchOfModelTest.java
@@ -0,0 +1,51 @@
+/*
+ * Created on 2015/01/28
+ * Copyright (C) 2015 Koga Laboratory. All rights reserved.
+ *
+ */
+package org.mklab.mikity.model.search;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertThat;
+
+/**
+ * LinkSearchOfModelのためのテストクラスです。
+ * @author soda
+ * @version $Revision$, 2015/01/28
+ */
+public class LinkSearchOfModelTest {
+  
+  private LinkSearchOfModel linkModel;
+  
+  /**
+   * 初期化メソッドです。
+   */
+  @Before
+  public void setUp() {
+    this.linkModel = new LinkSearchOfModel();
+  }
+  /**
+   * get,setColumnNumber()のテストメソッドです。
+   */
+  @Test
+  public void testGetSetColumnNumber() {
+    Integer column = new Integer(3);
+    this.linkModel.setColumnNumber(column.intValue());
+    Integer testColumn = new Integer(this.linkModel.getColumnNumber());
+    assertThat(column, equalTo(testColumn));
+  }
+  /**
+   * get,setTargetName()のテストメソッドです。
+   */
+  @Test
+  public void testGetSetTargetName() {
+    String target = "sinsi"; //$NON-NLS-1$
+    this.linkModel.setTargetName(target);
+    String testTarget = this.linkModel.getTartetName();
+    assertThat(target, equalTo(testTarget));
+  }
+
+}
