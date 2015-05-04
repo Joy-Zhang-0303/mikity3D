8b4514648d90b7b6536bc5034ea5924b8ea2fcdf
Masanobu Koga <koga@ces.kyutech.ac.jp>
2015-05-04 10:54:57 +0900

    Made it to use try-with-resources statement.

--- mikity3D-core/src/main/java/org/mklab/mikity/model/xml/AbstractMikity3DUnmarshaller.java
+++ mikity3D-core/src/main/java/org/mklab/mikity/model/xml/AbstractMikity3DUnmarshaller.java
@@ -10,6 +10,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
+
 /**
  * Mikity3DのUnmarshallerの抽象クラスです。１
  * 
@@ -17,18 +18,19 @@ import java.io.IOException;
  * @version $Revision$, 2013/02/05
  */
 public abstract class AbstractMikity3DUnmarshaller implements Mikity3dUnmashaller {
+
   /**
    * {@inheritDoc}
    */
   public void unmarshal(File file) throws IOException, Mikity3dSerializeDeserializeException {
-    final BufferedReader reader = new BufferedReader(new FileReader(file));
     final StringBuffer data = new StringBuffer();
 
-    String line;
-    while ((line = reader.readLine()) != null) {
-      data.append(line);
+    try (final BufferedReader reader = new BufferedReader(new FileReader(file))) {
+      String line;
+      while ((line = reader.readLine()) != null) {
+        data.append(line);
+      }
     }
-    reader.close();
 
     if (data.indexOf("<mikity3d") != -1) { //$NON-NLS-1$
       unmarshalFromMikity3DFile(file);
