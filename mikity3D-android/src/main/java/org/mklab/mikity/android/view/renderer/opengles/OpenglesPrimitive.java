22d56aa7ecfd3a44589697ae28f1be9c4c4ca9df
Masanobu Koga <koga@ces.kyutech.ac.jp>
2015-08-17 12:58:32 +0900

    Removed unnecessary classes.

--- mikity3D-android/src/main/java/org/mklab/mikity/android/view/renderer/opengles/OpenglesPrimitive.java
+++ mikity3D-android/src/main/java/org/mklab/mikity/android/view/renderer/opengles/OpenglesPrimitive.java
@@ -1,27 +1,36 @@
 /*
- * Created on 2013/02/12
- * Copyright (C) 2013 Koga Laboratory. All rights reserved.
+ * Created on 2015/08/02
+ * Copyright (C) 2015 Koga Laboratory. All rights reserved.
  *
  */
 package org.mklab.mikity.android.view.renderer.opengles;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+
 import javax.microedition.khronos.opengles.GL10;
 
+import org.mklab.mikity.model.graphic.GraphicObject;
 import org.mklab.mikity.model.graphic.GraphicPrimitive;
 import org.mklab.mikity.model.xml.simplexml.model.ColorModel;
 
 /**
- * OpenGL ESのプリミティブを表す抽象クラスです。
- * @author ohashi
- * @version $Revision$, 2013/02/12
+ * OpenGL ESのオブジェクトを表す抽象クラスです。
+ * 
+ * @author koga
+ * @version $Revision$, 2015/08/02
  */
-public class OpenglesPrimitive extends AbstractOpenglesObject {
+public class OpenglesPrimitive implements OpenglesObject {
+  /** グラフィックオブジェクト。 */
+  private GraphicObject object;
+  
   /**
-   * 新しく生成された<code>AbstractJoglObject</code>オブジェクトを初期化します。
+   * 新しく生成された<code>AbstractOpenglesObject</code>オブジェクトを初期化します。
    * @param object グラフィックオブジェクト
    */
-  public OpenglesPrimitive(GraphicPrimitive object) {
-    super(object);
+  public OpenglesPrimitive(GraphicObject object) {
+    this.object = object;
   }
   
   /**
@@ -59,22 +68,54 @@ public class OpenglesPrimitive extends AbstractOpenglesObject {
     }
   }
   
-//  /**
-//   * 色を設定します。
-//   * 
-//   * @param color 色
-//   */
-//  public void setColor(ColorModel color) {
-//    ((GraphicPrimitive)this.object).setColor(color);
-//  }
-//  
-//  /**
-//   * 透明性を設定します。
-//   * 
-//   * @param transparent 透明性
-//   */
-//  public void setTransparent(boolean transparent) {
-//    ((GraphicPrimitive)this.object).setTransparent(transparent);
-//  }
-}
+  /**
+   * 三角形ポリゴンを描画します。
+   * 
+   * @param gl GL
+   */
+  private void drawTrianglePolygons(GL10 gl) {
+    final float[] vertexArray = this.object.getVertexArray();
+    final float[] normalVectorArray = this.object.getNormalVectorArray();
+    
+    final FloatBuffer vertexBuffer = makeFloatBuffer(vertexArray);
+    final FloatBuffer normalBuffer = makeFloatBuffer(normalVectorArray);
+  
+    // 法線配列の有効化
+    gl.glEnableClientState(GL10.GL_NORMAL_ARRAY);
+    // 法線バッファの指定
+    gl.glNormalPointer(GL10.GL_FLOAT, 0, normalBuffer);
+    
+    // 頂点配列の有効化
+    gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
+    // 頂点バッファの指定
+    gl.glVertexPointer(3, GL10.GL_FLOAT, 0, vertexBuffer);
+  
+    final int number = vertexArray.length/3;
+    gl.glDrawArrays(GL10.GL_TRIANGLES, 0, number);
+  }
 
+  /**
+   * float配列をFloatBufferに変換します。
+   * 
+   * @param array 変換元
+   * @return 変換結果
+   */
+  private FloatBuffer makeFloatBuffer(float[] array) {
+    final FloatBuffer buffer = ByteBuffer.allocateDirect(array.length * 4).order(ByteOrder.nativeOrder()).asFloatBuffer();
+    buffer.put(array).position(0);
+    return buffer;
+  }
+
+  /**
+   * byte配列をByteBufferに変換します。
+   * 
+   * @param array 変換元
+   * @return 変換結果
+   */
+  @SuppressWarnings("unused")
+  private ByteBuffer makeByteBuffer(byte[] array) {
+    final ByteBuffer buffer = ByteBuffer.allocateDirect(array.length).order(ByteOrder.nativeOrder());
+    buffer.put(array).position(0);
+    return buffer;
+  }
+}
--- mikity3D-swt/src/main/java/org/mklab/mikity/view/renderer/jogl/JoglPrimitive.java
+++ mikity3D-swt/src/main/java/org/mklab/mikity/view/renderer/jogl/JoglPrimitive.java
@@ -1,29 +1,39 @@
 /*
- * Created on 2012/12/17
- * Copyright (C) 2012 Koga Laboratory. All rights reserved.
+ * Created on 2015/08/02
+ * Copyright (C) 2015 Koga Laboratory. All rights reserved.
  *
  */
 package org.mklab.mikity.view.renderer.jogl;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+
+import org.mklab.mikity.model.graphic.GraphicObject;
 import org.mklab.mikity.model.graphic.GraphicPrimitive;
 import org.mklab.mikity.model.xml.simplexml.model.ColorModel;
 
 import com.jogamp.opengl.GL;
 import com.jogamp.opengl.GL2;
+import com.jogamp.opengl.fixedfunc.GLPointerFunc;
+
 
 /**
- * JOGLのプリミティブを表すの抽象クラスです。
+ * JOGLのプリミティブを表すクラスです。
  * 
  * @author koga
- * @version $Revision$, 2012/12/17
+ * @version $Revision$, 2015/08/02
  */
-public class JoglPrimitive extends AbstractJoglObject {
+public class JoglPrimitive implements JoglObject {
+  /** グラフィックオブジェクト。 */
+  private GraphicObject object;
+  
   /**
    * 新しく生成された<code>AbstractJoglObject</code>オブジェクトを初期化します。
    * @param object グラフィックオブジェクト
    */
-  public JoglPrimitive(GraphicPrimitive object) {
-    super(object);
+  public JoglPrimitive(GraphicObject object) {
+    this.object = object;
   }
   
   /**
@@ -60,21 +70,54 @@ public class JoglPrimitive extends AbstractJoglObject {
     }
   }
   
-//  /**
-//   * 色を設定します。
-//   * 
-//   * @param color 色
-//   */
-//  public void setColor(ColorModel color) {
-//    ((GraphicPrimitive)this.object).setColor(color);
-//  }
-//  
-//  /**
-//   * 透明性を設定します。
-//   * 
-//   * @param transparent 透明性
-//   */
-//  public void setTransparent(boolean transparent) {
-//    ((GraphicPrimitive)this.object).setTransparent(transparent);
-//  }
+  /**
+   * 三角形ポリゴンを描画します。
+   * 
+   * @param gl GL
+   */
+  private void drawTrianglePolygons(GL2 gl) {
+    final float[] vertexArray = this.object.getVertexArray();
+    final float[] normalVectorArray = this.object.getNormalVectorArray();
+    
+    final FloatBuffer vertexBuffer = makeFloatBuffer(vertexArray);
+    final FloatBuffer normalBuffer = makeFloatBuffer(normalVectorArray);
+
+    // 法線配列の有効化
+    gl.glEnableClientState(GLPointerFunc.GL_NORMAL_ARRAY);
+    // 法線バッファの指定
+    gl.glNormalPointer(GL.GL_FLOAT, 0, normalBuffer);
+    
+    // 頂点配列の有効化
+    gl.glEnableClientState(GLPointerFunc.GL_VERTEX_ARRAY);
+    // 頂点バッファの指定
+    gl.glVertexPointer(3, GL.GL_FLOAT, 0, vertexBuffer);
+
+    final int number = vertexArray.length/3;
+    gl.glDrawArrays(GL.GL_TRIANGLES, 0, number);
+  }
+  
+  /**
+   * float配列をFloatBufferに変換します。
+   * 
+   * @param array 変換元
+   * @return 変換結果
+   */
+  private FloatBuffer makeFloatBuffer(float[] array) {
+    final FloatBuffer buffer = ByteBuffer.allocateDirect(array.length * 4).order(ByteOrder.nativeOrder()).asFloatBuffer();
+    buffer.put(array).position(0);
+    return buffer;
+  }
+
+  /**
+   * byte配列をByteBufferに変換します。
+   * 
+   * @param array 変換元
+   * @return 変換結果
+   */
+  @SuppressWarnings("unused")
+  private ByteBuffer makeByteBuffer(byte[] array) {
+    final ByteBuffer buffer = ByteBuffer.allocateDirect(array.length).order(ByteOrder.nativeOrder());
+    buffer.put(array).position(0);
+    return buffer;
+  }
 }
