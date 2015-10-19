5b4e87b14f6c6570202ee8abb5a5abec98530c96
hirae <hirae@mk.ces.kyutech.ac.jp>
2015-10-19 15:23:37 +0900

    CapsulePrimitive

--- mikity3D-core/src/main/java/org/mklab/mikity/model/graphic/CapsulePrimitive.java
+++ mikity3D-core/src/main/java/org/mklab/mikity/model/graphic/CapsulePrimitive.java
@@ -1,20 +1,19 @@
 package org.mklab.mikity.model.graphic;
 
 import org.mklab.mikity.model.xml.simplexml.model.CapsuleModel;
-import org.mklab.mikity.model.xml.simplexml.model.CylinderModel;
 import org.mklab.mikity.util.Vector3;
 
 
 /**
- * カプセルプリミティブを表すクラスです。
+ *　カプセルプリミティブを表すクラスです。
  * 
  * @author iwamoto
  * @version $Revision$, 2012/01/31
  */
 public class CapsulePrimitive extends AbstractGraphicPrimitive {
   /**
-   * 新しく生成された<code>CapsuleObject</code>オブジェクトを初期化します。
-   * @param capsule カプセル
+   * 新しく生成された<code>CylinderObject</code>オブジェクトを初期化します。
+   * @param cylinder シリンダー
    */
   public CapsulePrimitive(CapsuleModel capsule) {
     super(capsule);
@@ -36,7 +35,8 @@ public class CapsulePrimitive extends AbstractGraphicPrimitive {
     final int upperPolygonNumber = division;
     final int lowerPolygonNumber = division;
     final int sidePolygonNumber = division*2;
-    final int polygonNumber = upperPolygonNumber + lowerPolygonNumber + sidePolygonNumber;
+    final int spherePolygonNumber = division*division*2;
+    final int polygonNumber = upperPolygonNumber + lowerPolygonNumber + sidePolygonNumber + spherePolygonNumber;
     initializeArrays(polygonNumber);
 
     final float[] upperCenterPoint = new float[3];
@@ -71,10 +71,58 @@ public class CapsulePrimitive extends AbstractGraphicPrimitive {
     lowerPoints[division][1] = (float)(radius * Math.sin(0));
     lowerPoints[division][2] = -height / 2.0f;
     
+    //-------------------------------------------------------
+    final float incV = 2 * radius / division;
+    final float incH = 360.f / division;
+    
+    final float[][][] upperSpherePoints = new float[division/2 + 1][division + 1][3];
+    
+    for (int i = division/2 ; i >= 0; --i) {
+      final float z = (i) * incV + (height/2);
+      final float y = i * incV;
+      final float r = (float)Math.sqrt(radius * radius - y * y);
+      for (int j = division; j >= 0; --j) {
+        final float theta = (float)(j * incH * Math.PI / 180);
+        upperSpherePoints[i][j][0] = (float)(r * Math.cos(theta));
+        upperSpherePoints[i][j][1] = (float)(r * Math.sin(theta));
+        upperSpherePoints[i][j][2] = z;
+      }
+      
+      upperSpherePoints[i][division-1][0] = (float)(r * Math.cos(0));
+      upperSpherePoints[i][division-1][1] = (float)(r * Math.sin(0));
+      upperSpherePoints[i][division-1][2] = z;
+    }
+    
+
+    final float[][][] lowerSpherePoints = new float[division/2 + 1][division + 1][3];
+    
+    for (int i = 0; i < division/2; i++) {
+      final float z1 = (i+1) * incV - radius - (height/2);
+      final float y1 = i * incV - radius;
+      final float r1 = (float)Math.sqrt(radius * radius - y1 * y1);
+      for (int j = 0; j < division; ++j) {
+        final float theta1 = (float)(j * incH * Math.PI / 180);
+        lowerSpherePoints[i][j][0] = (float)(r1 * Math.cos(theta1));
+        lowerSpherePoints[i][j][1] = (float)(r1 * Math.sin(theta1));
+        lowerSpherePoints[i][j][2] = z1;
+      }
+      
+      lowerSpherePoints[i][division-1][0] = (float)(r1 * Math.cos(0));
+      lowerSpherePoints[i][division-1][1] = (float)(r1 * Math.sin(0));
+      lowerSpherePoints[i][division-1][2] = z1;
+    }
+    
+
     updateUpperPolygons(upperCenterPoint, upperPoints, division);
     updateLowerPolygons(lowerCenterPoint, lowerPoints, division);
     updateSidePolygons(upperPoints, lowerPoints, division);
+    updateLowerRightPolygons1(upperSpherePoints, division);
+    updateUpperLeftPolygons1(upperSpherePoints, division); 
+    updateLowerRightPolygons2(lowerSpherePoints, division);
+    updateUpperLeftPolygons2(lowerSpherePoints, division);
   }
+  
+  //=======================================================================================================
   
   /**
    * 側面のポリゴンを生成します。
@@ -188,4 +236,119 @@ public class CapsulePrimitive extends AbstractGraphicPrimitive {
       appendNormalVectorsOfTriangle(normalVector);
     }
   }
+
+  //-----------------------------------------------------------------------------
+/**
+ * 四角形セグメントの左上のポリゴンを更新します。
+ * 
+ * @param points 球面上の格子点
+ */
+private void updateUpperLeftPolygons1(final float[][][] upperSpherePoints, int division) {
+  for (int i = 0; i < division/2 - 1; ++i) {
+    for (int j = 0; j < division - 1; ++j) {
+      final float[][] vertices = new float[3][3];
+      vertices[0][0] = upperSpherePoints[i][j][0];
+      vertices[0][1] = upperSpherePoints[i][j][1];
+      vertices[0][2] = upperSpherePoints[i][j][2];
+
+      vertices[1][0] = upperSpherePoints[i+1][j+1][0];
+      vertices[1][1] = upperSpherePoints[i+1][j+1][1];
+      vertices[1][2] = upperSpherePoints[i+1][j+1][2];
+
+      vertices[2][0] = upperSpherePoints[i+1][j][0];
+      vertices[2][1] = upperSpherePoints[i+1][j][1];
+      vertices[2][2] = upperSpherePoints[i+1][j][2];
+
+      final Vector3 v0 = new Vector3(vertices[1][0] - vertices[0][0], vertices[1][1] - vertices[0][1], vertices[1][2] - vertices[0][2]);
+      final Vector3 v1 = new Vector3(vertices[2][0] - vertices[0][0], vertices[2][1] - vertices[0][1], vertices[2][2] - vertices[0][2]);
+      final float[] normalVector = v0.cross(v1).array();
+      
+      appendVertices(vertices);
+      appendNormalVectorsOfTriangle(normalVector);
+    }
+  }
 }
+private void updateUpperLeftPolygons2(final float[][][] lowerSpherePoints, int division) {
+	  for (int i = 0; i < division/2 -1; ++i) {
+	    for (int j = 0; j < division -1; ++j) {
+	      final float[][] vertices = new float[3][3];
+	      vertices[0][0] = lowerSpherePoints[i][j][0];
+	      vertices[0][1] = lowerSpherePoints[i][j][1];
+	      vertices[0][2] = lowerSpherePoints[i][j][2];
+
+	      vertices[1][0] = lowerSpherePoints[i+1][j+1][0];
+	      vertices[1][1] = lowerSpherePoints[i+1][j+1][1];
+	      vertices[1][2] = lowerSpherePoints[i+1][j+1][2];
+
+	      vertices[2][0] = lowerSpherePoints[i+1][j][0];
+	      vertices[2][1] = lowerSpherePoints[i+1][j][1];
+	      vertices[2][2] = lowerSpherePoints[i+1][j][2];
+
+	      final Vector3 v0 = new Vector3(vertices[1][0] - vertices[0][0], vertices[1][1] - vertices[0][1], vertices[1][2] - vertices[0][2]);
+	      final Vector3 v1 = new Vector3(vertices[2][0] - vertices[0][0], vertices[2][1] - vertices[0][1], vertices[2][2] - vertices[0][2]);
+	      final float[] normalVector = v0.cross(v1).array();
+	      
+	      appendVertices(vertices);
+	      appendNormalVectorsOfTriangle(normalVector);
+	    }
+	  }
+	}
+
+//-------------------------------------------------------------------------------------------
+/**
+ * 四角形セグメントの右下のポリゴンを更新します。
+ * 
+ * @param points 球面上の格子点
+ */
+private void updateLowerRightPolygons1(final float[][][] upperSpherePoints, int division) {
+  for (int i = 0; i < division/2; ++i) {
+    for (int j = 0; j < division -1; ++j) {
+      final float[][] vertices = new float[3][3];
+      vertices[0][0] = upperSpherePoints[i][j][0];
+      vertices[0][1] = upperSpherePoints[i][j][1];
+      vertices[0][2] = upperSpherePoints[i][j][2];
+
+      vertices[1][0] = upperSpherePoints[i][j+1][0];
+      vertices[1][1] = upperSpherePoints[i][j+1][1];
+      vertices[1][2] = upperSpherePoints[i][j+1][2];
+
+      vertices[2][0] = upperSpherePoints[i+1][j+1][0];
+      vertices[2][1] = upperSpherePoints[i+1][j+1][1];
+      vertices[2][2] = upperSpherePoints[i+1][j+1][2];
+
+      final Vector3 v0 = new Vector3(vertices[1][0] - vertices[0][0], vertices[1][1] - vertices[0][1], vertices[1][2] - vertices[0][2]);
+      final Vector3 v1 = new Vector3(vertices[2][0] - vertices[0][0], vertices[2][1] - vertices[0][1], vertices[2][2] - vertices[0][2]);
+      final float[] normalVector = v0.cross(v1).array();
+      appendVertices(vertices);
+      appendNormalVectorsOfTriangle(normalVector);
+    }
+  }
+}
+
+private void updateLowerRightPolygons2(final float[][][] lowerSpherePoints, int division) {
+	  for (int i = 0; i < division/2 -1; ++i) {
+	    for (int j = 0; j < division -1; ++j) {
+	      final float[][] vertices = new float[3][3];
+	      vertices[0][0] = lowerSpherePoints[i][j][0];
+	      vertices[0][1] = lowerSpherePoints[i][j][1];
+	      vertices[0][2] = lowerSpherePoints[i][j][2];
+
+	      vertices[1][0] = lowerSpherePoints[i][j+1][0];
+	      vertices[1][1] = lowerSpherePoints[i][j+1][1];
+	      vertices[1][2] = lowerSpherePoints[i][j+1][2];
+
+	      vertices[2][0] = lowerSpherePoints[i+1][j+1][0];
+	      vertices[2][1] = lowerSpherePoints[i+1][j+1][1];
+	      vertices[2][2] = lowerSpherePoints[i+1][j+1][2];
+
+	      final Vector3 v0 = new Vector3(vertices[1][0] - vertices[0][0], vertices[1][1] - vertices[0][1], vertices[1][2] - vertices[0][2]);
+	      final Vector3 v1 = new Vector3(vertices[2][0] - vertices[0][0], vertices[2][1] - vertices[0][1], vertices[2][2] - vertices[0][2]);
+	      final float[] normalVector = v0.cross(v1).array();
+	      appendVertices(vertices);
+	      appendNormalVectorsOfTriangle(normalVector);
+	    }
+	  }
+	}
+}
+
+
