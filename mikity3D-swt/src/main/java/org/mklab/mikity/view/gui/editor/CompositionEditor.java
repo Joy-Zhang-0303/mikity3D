aecd38248fa9d9a28de7ecddb7ca9a2675865664
Masanobu Koga <koga@ces.kyutech.ac.jp>
2015-12-01 00:50:18 +0900

    Now, we can change the width, height, and depth of the composition object with preserving ratio.

--- mikity3D-swt/src/main/java/org/mklab/mikity/view/gui/editor/CompositionEditor.java
+++ mikity3D-swt/src/main/java/org/mklab/mikity/view/gui/editor/CompositionEditor.java
@@ -6,6 +6,9 @@
 package org.mklab.mikity.view.gui.editor;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
@@ -29,6 +32,8 @@ public class CompositionEditor extends AbstractObjectEditor {
   private ParameterInputBox heightValue;
   /** 奥行き。 */
   private ParameterInputBox depthValue;
+  /** 比率保存用ボタン。 */
+  private Button preservingRatio;
 
   /**
    * 新しく生成された<code>CompositionEditor</code>オブジェクトを初期化します。
@@ -53,32 +58,73 @@ public class CompositionEditor extends AbstractObjectEditor {
     sizeValue.setEditable(false);
        
     this.widthValue = new ParameterInputBox(parameterGroup, this, SWT.NONE, Messages.getString("CompositionEditor.2"), "" + compostion.getWidth()); //$NON-NLS-1$ //$NON-NLS-2$
-    this.widthValue.setEditable(false);
 
     final Label widthUnit = new Label(parameterGroup, SWT.NONE);
     widthUnit.setText(UnitLabel.getUnit("modelLength")); //$NON-NLS-1$
     setGridLayout(widthUnit, 1);
     
     this.heightValue = new ParameterInputBox(parameterGroup, this, SWT.NONE, Messages.getString("CompositionEditor.3"), "" + compostion.getHeight()); //$NON-NLS-1$ //$NON-NLS-2$
-    this.heightValue.setEditable(false);
 
     final Label heightUnit = new Label(parameterGroup, SWT.NONE);
     heightUnit.setText(UnitLabel.getUnit("modelLength")); //$NON-NLS-1$
     setGridLayout(heightUnit, 1);
     
     this.depthValue = new ParameterInputBox(parameterGroup, this, SWT.NONE, Messages.getString("CompositionEditor.5"), "" + compostion.getDepth()); //$NON-NLS-1$ //$NON-NLS-2$
-    this.depthValue.setEditable(false);
 
     final Label depthUnit = new Label(parameterGroup, SWT.NONE);
     depthUnit.setText(UnitLabel.getUnit("modelLength")); //$NON-NLS-1$
     setGridLayout(depthUnit, 1);
+    
+    final Label preservingRatioLabel = new Label(parameterGroup, SWT.NONE);
+    preservingRatioLabel.setText(Messages.getString("CompositionEditor.4")); //$NON-NLS-1$
+    setGridLayout(preservingRatioLabel, 1);
+    
+    this.preservingRatio = new Button(parameterGroup, SWT.CHECK);
+    this.preservingRatio.setSelection(true);
+    this.preservingRatio.addSelectionListener(new SelectionListener() {
+
+      @Override
+      public void widgetDefaultSelected(@SuppressWarnings("unused") SelectionEvent e) {
+        // nothing to do
+      }
+
+      @Override
+      public void widgetSelected(@SuppressWarnings("unused") SelectionEvent e) {
+        modifyText(null);
+      }
+    });
   }
 
   /**
    * {@inheritDoc}
    */
   public void updateModelParameters() {
-    // nothing to do
+    final float width =  this.widthValue.getFloatValue();
+    final float compositionWidth = ((CompositionModel)this.object).getWidth();
+
+    final float height =  this.heightValue.getFloatValue();
+    final float compositionHeight = ((CompositionModel)this.object).getHeight();
+    
+    final float depth =  this.depthValue.getFloatValue();
+    final float compositionDepth = ((CompositionModel)this.object).getDepth();
+
+    if (width != compositionWidth || height != compositionHeight || depth != compositionDepth) {
+      if (this.preservingRatio.getSelection() == false) {
+        ((CompositionModel)this.object).scale(depth/compositionDepth, width/compositionWidth, height/compositionHeight);
+      } else {
+        float scale = 1;
+        if (height != compositionHeight) {
+          scale = height/compositionHeight;
+        }
+        if (depth != compositionDepth) {
+          scale = depth/compositionDepth;
+        }
+        if (width != compositionWidth) {
+          scale = width/compositionWidth;
+        }
+        ((CompositionModel)this.object).scale(scale, scale, scale);
+      }
+    }
   }
 
   /**
