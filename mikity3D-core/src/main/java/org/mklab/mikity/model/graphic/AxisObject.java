6ed4686650c4700272c02052975890ba5a491913
Masanobu Koga <koga@ces.kyutech.ac.jp>
2015-12-03 16:15:01 +0900

    Modified the header of axis.

--- mikity3D-core/src/main/java/org/mklab/mikity/model/graphic/AxisObject.java
+++ mikity3D-core/src/main/java/org/mklab/mikity/model/graphic/AxisObject.java
@@ -24,56 +24,48 @@ public class AxisObject extends AbstractGraphicObject {
     updatePolygons();
   }
   
+  /**
+   * ポリゴンを更新します。
+   */
   private void updatePolygons() {
-    float radius = ((AxisModel)this.object).getRadius();
-    float height = ((AxisModel)this.object).getHeight();
-    int division = ((AxisModel)this.object).getDivision();
+    final float radius = ((AxisModel)this.object).getRadius();
+    final float height = ((AxisModel)this.object).getHeight();
+    final int division = ((AxisModel)this.object).getDivision();
     
     if (radius == 0 || height == 0 || division == 0) {
       return;
     }
     
-    final int upperCylinderPolygonNumber = division;
-    final int lowerCylinderPolygonNumber = division;
-    final int sideCylinderPolygonNumber = division*2;
-    final int cylinderPolygonNumber = upperCylinderPolygonNumber + lowerCylinderPolygonNumber + sideCylinderPolygonNumber;
+    final int bodyUpperPolygonNumber = division;
+    final int bodyLowerPolygonNumber = division;
+    final int bodySidePolygonNumber = division*2;
+    final int bodyPolygonNumber = bodyUpperPolygonNumber + bodyLowerPolygonNumber + bodySidePolygonNumber;
     
-    final int lowerConePolygonNumber = division;
-    final int sideConePolygonNumber = division;
-    final int conePolygonNumber = lowerConePolygonNumber + sideConePolygonNumber;
+    final int headerLowerPolygonNumber = division;
+    final int headerSidePolygonNumber = division;
+    final int headerPolygonNumber = headerLowerPolygonNumber + headerSidePolygonNumber;
     
-    final int polygonNumber = cylinderPolygonNumber + conePolygonNumber;
+    final int polygonNumber = bodyPolygonNumber + headerPolygonNumber;
     initializeArrays(polygonNumber);
     
-    final float[] coneTopPoints = new float[3];
-    coneTopPoints[0] = 0;
-    coneTopPoints[1] = 0;
-    coneTopPoints[2] = height;
-    
-    final float[] coneCenterPoints = new float[3];
-    coneCenterPoints[0] = 0;
-    coneCenterPoints[1] = 0;
-    coneCenterPoints[2] = height - height / 6.0f;
-    
-    final float[][] coneLowPoints = new float[division+1][3];
-    
-    for (int i = 0; i <= division; i++) {
-      final double theta = 2.0 * Math.PI / division * i;
-      coneLowPoints[i][0] = radius * (float)Math.cos(theta) * 3;
-      coneLowPoints[i][1] = radius * (float)Math.sin(theta) * 3; 
-      coneLowPoints[i][2] =  height - height / 6.0f;
-    }
-    coneLowPoints[division][0] = radius * (float)Math.cos(0) * 3;
-    coneLowPoints[division][1] = radius * (float)Math.sin(0) * 3; 
-    coneLowPoints[division][2] =  height - height / 6.0f;
-    
-    updateLowerPolygons(coneCenterPoints, coneLowPoints, division);
-    updateConeSidePolygons(coneTopPoints, coneLowPoints, division);
+    updateHeaderPolygons(radius, height, division);
+    updateBodyPolygons(radius, height, division);
+  }
+
+  /**
+   * 軸本体のポリゴンを更新します。
+   * 
+   * @param radius 半径
+   * @param height 高さ
+   * @param division 分割数
+   */
+  private void updateBodyPolygons(final float radius, final float height, final int division) {
+    final float headerHeight = 6 * radius;
     
     final float[] cylinderUpperCenterPoint = new float[3];
     cylinderUpperCenterPoint[0] = 0;
     cylinderUpperCenterPoint[1] = 0;
-    cylinderUpperCenterPoint[2] =  height - height / 6.0f;
+    cylinderUpperCenterPoint[2] =  height - headerHeight;
     
     final float[] cylinderLowerCenterPoint = new float[3];
     cylinderLowerCenterPoint[0] = 0;
@@ -85,11 +77,11 @@ public class AxisObject extends AbstractGraphicObject {
       final double theta = 2.0 * Math.PI / division * i;
       cylinderUpperPoints[i][0] = (float)(radius * Math.cos(theta));
       cylinderUpperPoints[i][1] = (float)(radius * Math.sin(theta));
-      cylinderUpperPoints[i][2] =  height - height / 6.0f;
+      cylinderUpperPoints[i][2] =  height - headerHeight;
     }
     cylinderUpperPoints[division][0] = (float)(radius * Math.cos(0));
     cylinderUpperPoints[division][1] = (float)(radius * Math.sin(0));
-    cylinderUpperPoints[division][2] =  height - height / 6.0f;
+    cylinderUpperPoints[division][2] =  height - headerHeight;
 
     final float[][] cylinderLowerPoints = new float[division+1][3];
     for (int i = 0; i < division; i++) {
@@ -102,31 +94,68 @@ public class AxisObject extends AbstractGraphicObject {
     cylinderLowerPoints[division][1] = (float)(radius * Math.sin(0));
     cylinderLowerPoints[division][2] = 0;
     
-    updateLowerPolygons(cylinderLowerCenterPoint, cylinderLowerPoints, division);
-    updateCylinderUpperPolygons(cylinderUpperCenterPoint, cylinderUpperPoints, division);
-    updateCylinderSidePolygons(cylinderUpperPoints, cylinderLowerPoints, division);
+    updateLowerDiskPolygons(cylinderLowerCenterPoint, cylinderLowerPoints, division);
+    updateUpperDiskPolygons(cylinderUpperCenterPoint, cylinderUpperPoints, division);
+    updateBodySidePolygons(cylinderUpperPoints, cylinderLowerPoints, division);
+  }
+
+  /**
+   * 軸先端のポリゴンを更新します。
+   * 
+   * @param radius 半径
+   * @param height 高さ
+   * @param division 分割数
+   */
+  private void updateHeaderPolygons(final float radius, final float height, final int division) {
+    final float headerRadius = 3 * radius;    
+    final float headerHeight = 6 * radius;
+    
+    final float[] coneTopPoints = new float[3];
+    coneTopPoints[0] = 0;
+    coneTopPoints[1] = 0;
+    coneTopPoints[2] = height;
+    
+    final float[] coneCenterPoints = new float[3];
+    coneCenterPoints[0] = 0;
+    coneCenterPoints[1] = 0;
+    coneCenterPoints[2] = height - headerHeight;
+    
+    final float[][] coneLowerPoints = new float[division+1][3];
+    
+    for (int i = 0; i <= division; i++) {
+      final double theta = 2.0 * Math.PI / division * i;
+      coneLowerPoints[i][0] = headerRadius * (float)Math.cos(theta);
+      coneLowerPoints[i][1] = headerRadius * (float)Math.sin(theta); 
+      coneLowerPoints[i][2] = height - headerHeight;
+    }
+    coneLowerPoints[division][0] = headerRadius * (float)Math.cos(0);
+    coneLowerPoints[division][1] = headerRadius * (float)Math.sin(0); 
+    coneLowerPoints[division][2] = height - headerHeight;
+    
+    updateLowerDiskPolygons(coneCenterPoints, coneLowerPoints, division);
+    updateHeaderSidePolygons(coneTopPoints, coneLowerPoints, division);
   }
   
   /**
-   * 上面のポリゴンを更新します。
+   * 円盤の上面のポリゴンを更新します。
    * 
-   * @param upperCenterPoint 上面の中心
-   * @param upperPoints 上面の周囲の頂点
+   * @param centerPoint 円盤の中心
+   * @param surroundingPoints 周囲の頂点
    */
-  private void updateCylinderUpperPolygons(final float[] upperCenterPoint, final float[][] upperPoints, int division) {
+  private void updateUpperDiskPolygons(final float[] centerPoint, final float[][] surroundingPoints, int division) {
     for (int i = 0; i < division; i++) {
       final float[][] vertices = new float[3][3];
-      vertices[0][0] = upperCenterPoint[0];
-      vertices[0][1] = upperCenterPoint[1];
-      vertices[0][2] = upperCenterPoint[2];
+      vertices[0][0] = centerPoint[0];
+      vertices[0][1] = centerPoint[1];
+      vertices[0][2] = centerPoint[2];
       
-      vertices[1][0] = upperPoints[i][0];
-      vertices[1][1] = upperPoints[i][1];
-      vertices[1][2] = upperPoints[i][2];
+      vertices[1][0] = surroundingPoints[i][0];
+      vertices[1][1] = surroundingPoints[i][1];
+      vertices[1][2] = surroundingPoints[i][2];
 
-      vertices[2][0] = upperPoints[i+1][0];
-      vertices[2][1] = upperPoints[i+1][1];
-      vertices[2][2] = upperPoints[i+1][2];
+      vertices[2][0] = surroundingPoints[i+1][0];
+      vertices[2][1] = surroundingPoints[i+1][1];
+      vertices[2][2] = surroundingPoints[i+1][2];
 
       final Vector3 v0 = new Vector3(vertices[1][0] - vertices[0][0], vertices[1][1] - vertices[0][1], vertices[1][2] - vertices[0][2]);
       final Vector3 v1 = new Vector3(vertices[2][0] - vertices[0][0], vertices[2][1] - vertices[0][1], vertices[2][2] - vertices[0][2]);
@@ -143,7 +172,7 @@ public class AxisObject extends AbstractGraphicObject {
    * @param upperPoints 上面の周囲の頂点
    * @param lowerPoints 下面の周囲の頂点
    */
-  private void updateCylinderSidePolygons(final float[][] upperPoints, final float[][] lowerPoints, int division) {
+  private void updateBodySidePolygons(final float[][] upperPoints, final float[][] lowerPoints, int division) {
     for (int i = 0; i < division; i++) {
       final float[][] vertices = new float[3][3];
       vertices[0][0] = upperPoints[i][0];
@@ -192,10 +221,11 @@ public class AxisObject extends AbstractGraphicObject {
   
   /**
    * 側面のポリゴンを更新します。
+   * 
    * @param topPoint 上の点
    * @param lowerPoints 底面の周囲の頂点
    */
-  private void updateConeSidePolygons(final float[] topPoint, final float[][] lowerPoints, int division) {
+  private void updateHeaderSidePolygons(final float[] topPoint, final float[][] lowerPoints, int division) {
     for (int i = 0; i < division; i++) {
       final float[][] vertices = new float[3][3];
       vertices[0][0] = topPoint[0];
@@ -220,25 +250,25 @@ public class AxisObject extends AbstractGraphicObject {
   }
   
   /**
-   * 底面のポリゴンを更新します。
+   * 円盤の下面のポリゴンを更新します。
    * 
-   * @param centerPoint 底面の中心
-   * @param lowerPoints 底面の周囲の頂点
+   * @param centerPoint 円盤の中心
+   * @param surroundingPoints 底面の周囲の頂点
    */
-  private void updateLowerPolygons(final float[] centerPoint, final float[][] lowerPoints, int division) {
+  private void updateLowerDiskPolygons(final float[] centerPoint, final float[][] surroundingPoints, int division) {
     for (int i = 0; i < division; i++) {
       final float[][] vertices = new float[3][3];
       vertices[0][0] = centerPoint[0];
       vertices[0][1] = centerPoint[1];
       vertices[0][2] = centerPoint[2];
       
-      vertices[1][0] = lowerPoints[i+1][0];
-      vertices[1][1] = lowerPoints[i+1][1];
-      vertices[1][2] = lowerPoints[i+1][2];
+      vertices[1][0] = surroundingPoints[i+1][0];
+      vertices[1][1] = surroundingPoints[i+1][1];
+      vertices[1][2] = surroundingPoints[i+1][2];
 
-      vertices[2][0] = lowerPoints[i][0];
-      vertices[2][1] = lowerPoints[i][1];
-      vertices[2][2] = lowerPoints[i][2];
+      vertices[2][0] = surroundingPoints[i][0];
+      vertices[2][1] = surroundingPoints[i][1];
+      vertices[2][2] = surroundingPoints[i][2];
 
       final Vector3 v0 = new Vector3(vertices[1][0] - vertices[0][0], vertices[1][1] - vertices[0][1], vertices[1][2] - vertices[0][2]);
       final Vector3 v1 = new Vector3(vertices[2][0] - vertices[0][0], vertices[2][1] - vertices[0][1], vertices[2][2] - vertices[0][2]);
