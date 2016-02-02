71d29348113f933145cb64c77b94ca14ae8c7e11
hirae <hirae@mk.ces.kyutech.ac.jp>
2016-02-02 13:49:12 +0900

    create package named "adapter" in "settings" and create java file named "SettingsDrawerListAdapter.java"

---
+++ mikity3D-android/src/main/java/org/mklab/mikity/android/settings/adapter/SettingsDrawerListAdapter.java
@@ -0,0 +1,82 @@
+package org.mklab.mikity.android.settings.adapter;
+
+import java.util.ArrayList;
+
+import org.mklab.mikity.android.R;
+import org.mklab.mikity.android.settings.SettingsDrawerItem;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * @author 
+ * @version $Revision$, 2016/02/01
+ */
+public class SettingsDrawerListAdapter extends BaseAdapter {
+
+  private Context context;
+  private ArrayList<SettingsDrawerItem> items;
+
+  /**
+   * 新しく生成された<code>NavDrawerListAdapter</code>オブジェクトを初期化します。
+   * 
+   * @param context コンテキスト
+   * @param items 項目
+   */
+  public SettingsDrawerListAdapter(Context context, ArrayList<SettingsDrawerItem> items) {
+    this.context = context;
+    this.items = items;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public int getCount() {
+    return this.items.size();
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public Object getItem(int position) {
+    return this.items.get(position);
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public long getItemId(int position) {
+    return position;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public View getView(int position, View convertView, ViewGroup parent) {
+    if (convertView == null) {
+      LayoutInflater mInflater = (LayoutInflater)this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+      convertView = mInflater.inflate(R.layout.drawer_list_item, null);
+    }
+
+    ImageView icon = (ImageView)convertView.findViewById(R.id.navicon);
+    TextView title = (TextView)convertView.findViewById(R.id.title);
+    TextView counter = (TextView)convertView.findViewById(R.id.counter);
+
+    icon.setImageResource(this.items.get(position).getIcon());
+    title.setText(this.items.get(position).getTitle());
+
+    if (this.items.get(position).getCounterVisiblity()) {
+      counter.setText(this.items.get(position).getCount());
+    } else {
+      counter.setVisibility(View.GONE);
+    }
+
+    return convertView;
+
+  }
+}
